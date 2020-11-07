577017c3d068eacb8af8ec2f3308aa5b3d291f9b
Matt Carlos <catmatlos@icloud.com>
2020-11-07 01:37:02 -0800

    updated map default zoom

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ function initMap() {
     const csc = { lat: 38.107643, lng: -122.569841};
     // The map, centered at csc
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
+      zoom: 16,
       center: csc,
     });
     // The marker, positioned at csc
